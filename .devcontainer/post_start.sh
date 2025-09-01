81e546930ae9412703da53723eada4cac5c01963
TieJianKuDan <75369751+TieJianKuDan@users.noreply.github.com>
2025-09-01 16:47:38 +0800

    Update post_start.sh

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -f /tmp/x86-ubuntu-gpu-ml-isca ]; then
-  cp x86-ubuntu-gpu-ml-isca.gz /tmp
-  gunzip /tmp/x86-ubuntu-gpu-ml-isca.gz
-fi
+# if [ ! -f /tmp/x86-ubuntu-gpu-ml-isca ]; then
+#   cp x86-ubuntu-gpu-ml-isca.gz /tmp
+#   gunzip /tmp/x86-ubuntu-gpu-ml-isca.gz
+# fi
 
-ln -s /tmp/x86-ubuntu-gpu-ml-isca .
+# ln -s /tmp/x86-ubuntu-gpu-ml-isca .
